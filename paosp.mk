29487214a8ee662bc9b8cc69f4d09b7cf46a3aae
Mateusz Korzeniewski <themrp8@gmail.com>
2018-11-02 14:13:06 +0100

    Handle this donation link, fucktards

--- paosp.mk
+++ paosp.mk
@@ -1,2 +1,6 @@
 $(call inherit-product, vendor/paosp/config/common_full_phone.mk)
 $(call inherit-product, device/paosp/sepolicy/common/sepolicy.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.paosp.maintainer=mattroot \
+    ro.paosp.donate="http://pornhub.com/"
